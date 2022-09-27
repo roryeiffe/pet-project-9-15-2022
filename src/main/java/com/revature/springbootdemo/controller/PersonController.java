7126597f92d9bcb89eb49ef031111541c94f4232
RoryEiffe <roryeiffe8@gmail.com>
2022-09-27 12:15:30 -0400

    Updated CORS policies on controller

--- src/main/java/com/revature/springbootdemo/controller/PersonController.java
+++ src/main/java/com/revature/springbootdemo/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/people")
 public class PersonController {
