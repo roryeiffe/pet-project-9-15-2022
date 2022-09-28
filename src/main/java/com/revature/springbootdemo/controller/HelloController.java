6d99ac0c30e32c590c5b1fc491a27640c7fbfa4a
RoryEiffe <roryeiffe8@gmail.com>
2022-09-28 11:40:02 -0400

    Updated CORS on Hello Controller

--- src/main/java/com/revature/springbootdemo/controller/HelloController.java
+++ src/main/java/com/revature/springbootdemo/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.revature.springbootdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
